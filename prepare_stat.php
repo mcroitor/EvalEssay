57a27d7b12257ff9b5f9c76e0c01c5fca3e210c7
Mihail Croitor <mihail.croitor@usm.md>
2025-11-24 19:37:03 +0200

    extended stat

--- prepare_stat.php
+++ prepare_stat.php
@@ -99,14 +99,19 @@ function toMarkdown(array $data, string $model): string {
     $lines[] = "## Summary Statistics";
     $lines[] = "";
     $allScores = [];
-    $lines[] = "| Essay | Number of Assessments | Average Score | Min Score | Max Score |";
-    $lines[] = "|-------|-----------------------|---------------|-----------|-----------|";
+    $lines[] = "| Essay | Number of Assessments | Average Score | Min Score | Median |  Max Score | Deviation |";
+    $lines[] = "|-------|-----------------------|---------------|-----------|--------|------------|-----------|";
     foreach ($data as $essay => $scores) {
         $numAssessments = count($scores);
         $averageScore = $numAssessments > 0 ? array_sum($scores) / $numAssessments : 0;
         $minScore = $numAssessments > 0 ? min($scores) : 0;
         $maxScore = $numAssessments > 0 ? max($scores) : 0;
-        $lines[] = "| {$essay} | {$numAssessments} | " . number_format($averageScore, 2) . " | {$minScore} | {$maxScore} |";
+        sort($scores);
+        $median = $numAssessments % 2 == 0
+            ? ($scores[$numAssessments / 2 - 1] + $scores[$numAssessments / 2]) / 2
+            : $scores[floor($numAssessments / 2)];
+        $deviation = sqrt(array_sum(array_map(fn($x) => pow($x - $averageScore, 2), $scores)) / $numAssessments);
+        $lines[] = "| {$essay} | {$numAssessments} | " . number_format($averageScore, 2) . " | {$minScore} | {$median} | {$maxScore} | " . number_format($deviation, 2) . " |";
         $allScores = array_merge($allScores, $scores);
     }
 
